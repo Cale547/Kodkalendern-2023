20966c2bb40f4d9a2a1b382ec356dd19acc7fec0
Cale547 <119942117+Cale547@users.noreply.github.com>
2024-07-28 23:02:55 +0200

    gjort uppgifterna i python

---
+++ python/lucka1.py
@@ -0,0 +1,11 @@
+cellNumber = 0
+for i in range(127, 268):
+    cellNumber += i
+
+for i in range(1110, 1379):
+    cellNumber += i
+
+for i in range(3239293, 3239331):
+    cellNumber += i
+
+print("Tomtens nummer är", cellNumber)
